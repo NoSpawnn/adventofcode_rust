21daea5fab24ec97e7b383d71f05ebde7f4465d1
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2025-01-22 21:57:37 +0000

    start day 4

--- 2024/src/bin/04.rs
+++ 2024/src/bin/04.rs
@@ -2,10 +2,34 @@
 
 use std::fs::read_to_string;
 
+const XMAS: &str = "XMAS";
+const SAMX: &str = "SAMX";
+
 fn main() {
-    let input = read_to_string("inputs/day04.in")
+    let lines = read_to_string("inputs/day04.in")
         .unwrap()
         .lines()
         .map(|l| l.to_owned())
         .collect::<Vec<_>>();
+
+    println!("{}", lines.iter().map(|row| transpose(&row)).sum::<i32>());
+}
+
+fn count_in_row(row: &str) -> i32 {
+    let f_matches = row.match_indices(XMAS).count();
+    let r_matches = row.match_indices(SAMX).count();
+
+    (f_matches + r_matches) as i32
+}
+
+fn transpose(lines: &[&str]) -> Vec<String> {
+    let mut transposed = vec![String::new(); lines.len()];
+
+    for line in lines.iter() {
+        for (i, c) in line.chars().enumerate() {
+            transposed[i].push(c);
+        }
+    }
+
+    transposed
 }
