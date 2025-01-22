1c748523203c420e748ec5ec4d1ee0abe8754b9c
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2025-01-22 23:54:20 +0000

    Delete 2024/target directory

--- 2024/target/debug/deps/01-54d4b21f57210532.d
+++
@@ -1,5 +0,0 @@
-/home/red/Documents/programming/adventofcode_rust/2024/target/debug/deps/01-54d4b21f57210532: src/bin/01.rs
-
-/home/red/Documents/programming/adventofcode_rust/2024/target/debug/deps/01-54d4b21f57210532.d: src/bin/01.rs
-
-src/bin/01.rs:
--- 2024/target/debug/deps/aoc-bd74984e19d3ce12.d
+++
@@ -1,5 +0,0 @@
-/home/red/Documents/programming/adventofcode_rust/2024/target/debug/deps/libaoc-bd74984e19d3ce12.rmeta: src/main.rs
-
-/home/red/Documents/programming/adventofcode_rust/2024/target/debug/deps/aoc-bd74984e19d3ce12.d: src/main.rs
-
-src/main.rs:
