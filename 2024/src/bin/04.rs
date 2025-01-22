11fcfb519b062d88d0d8fd59e96f514abce392eb
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2025-01-22 23:52:18 +0000

    day 4

--- 2024/src/bin/04.rs
+++ 2024/src/bin/04.rs
@@ -2,8 +2,8 @@
 
 use std::fs::read_to_string;
 
-const XMAS: &str = "XMAS";
-const SAMX: &str = "SAMX";
+const CARDINAL: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+const CORNERS: [(i32, i32); 4] = [(-1, -1), (-1, 1), (1, 1), (1, -1)];
 
 fn main() {
     let lines = read_to_string("inputs/day04.in")
@@ -11,25 +11,56 @@ fn main() {
         .lines()
         .map(|l| l.to_owned())
         .collect::<Vec<_>>();
+    let directions = &[CARDINAL, CORNERS].concat();
 
-    println!("{}", lines.iter().map(|row| transpose(&row)).sum::<i32>());
-}
-
-fn count_in_row(row: &str) -> i32 {
-    let f_matches = row.match_indices(XMAS).count();
-    let r_matches = row.match_indices(SAMX).count();
-
-    (f_matches + r_matches) as i32
-}
-
-fn transpose(lines: &[&str]) -> Vec<String> {
-    let mut transposed = vec![String::new(); lines.len()];
-
-    for line in lines.iter() {
-        for (i, c) in line.chars().enumerate() {
-            transposed[i].push(c);
+    let mut p1 = 0;
+    let mut p2 = 0;
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.char_indices() {
+            if c == 'X' {
+                p1 += directions
+                    .iter()
+                    .filter(|&&dir| search(&lines, "XMAS", x as i32, y as i32, dir))
+                    .count();
+            } else if c == 'A' {
+                p2 += match CORNERS
+                    .iter()
+                    .map(|(dx, dy)| safe_get_at(&lines, x as i32 + *dx, y as i32 + *dy))
+                    .collect::<String>()
+                    .as_str()
+                {
+                    "MMSS" | "MSSM" | "SSMM" | "SMMS" => 1,
+                    _ => 0,
+                }
+            }
         }
     }
 
-    transposed
+    println!("2024 Day 03:");
+    println!("  Part 1: {p1}");
+    println!("  Part 2: {p2}");
+}
+
+fn search(haystack: &Vec<String>, needle: &str, x: i32, y: i32, dir: (i32, i32)) -> bool {
+    if needle.is_empty() {
+        return true;
+    }
+
+    if char_at(needle, 0) != safe_get_at(haystack, x, y) {
+        return false;
+    }
+
+    search(haystack, &needle[1..], x + dir.0, y + dir.1, dir)
+}
+
+fn char_at(s: &str, i: usize) -> char {
+    s.chars().nth(i).unwrap()
+}
+
+fn safe_get_at(grid: &Vec<String>, x: i32, y: i32) -> char {
+    if (y as usize) < grid.len() && (x as usize) < grid.get(0).unwrap().len() {
+        char_at(&grid[y as usize], x as usize)
+    } else {
+        ' '
+    }
 }
