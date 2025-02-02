401b777055cf9f3a0e41fa22ba0cb2b1679a5541
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2025-02-02 16:45:57 +0000

    Day 7 part 2

--- 2024/src/bin/07.rs
+++ 2024/src/bin/07.rs
@@ -2,10 +2,21 @@
 
 use std::{collections::HashMap, fs::read_to_string, time};
 
-#[derive(Debug)]
-enum Operation {
+#[derive(Debug, PartialEq, Eq)]
+enum Operator {
     Add,
     Mult,
+    Concat,
+}
+
+impl Operator {
+    fn operate(&self, lhs: u64, rhs: u64) -> u64 {
+        match self {
+            Operator::Add => lhs + rhs,
+            Operator::Mult => lhs * rhs,
+            Operator::Concat => format!("{}{}", lhs, rhs).parse().unwrap(),
+        }
+    }
 }
 
 fn main() {
@@ -13,31 +24,54 @@ fn main() {
     let start = time::Instant::now();
 
     let mut p1 = 0;
+    let mut p2 = 0;
     for line in input.lines() {
         let (target, nums) = line.split_once(':').unwrap();
         let target: u64 = target.parse().unwrap();
         let nums: Vec<u64> = nums.split_whitespace().flat_map(|n| n.parse()).collect();
         let len = nums.len();
+        let mut updated_p1 = false;
+        let mut updated_p2 = false;
 
-        // Generate all cases - https://www.reddit.com/r/rust/comments/91h6t8/generating_all_possible_case_variations_of_a/
-        for i in 0..u64::pow(2, len as u32) {
-            let mut acc = 0;
-            for (idx, n) in nums.iter().enumerate() {
-                if (i & (1 << idx)) == 0 {
-                    acc += n;
-                } else {
-                    acc *= n;
-                }
+        // Generate all possible equations - https://www.reddit.com/r/rust/comments/91h6t8/generating_all_possible_case_variations_of_a/
+        for i in 0..u64::pow(3, len as u32) {
+            let mut steps = i;
+            let mut has_concat = false;
+            let mut case = Vec::with_capacity(len);
+            for _ in 0..len {
+                case.push(match steps % 3 {
+                    0 => Operator::Add,
+                    1 => Operator::Mult,
+                    2 => {
+                        has_concat = true;
+                        Operator::Concat
+                    }
+                    _ => unreachable!(),
+                });
+                steps /= 3;
             }
 
-            if acc == target {
+            let valid = case
+                .iter()
+                .enumerate()
+                .fold(0u64, |acc, (i, op)| op.operate(acc, nums[i]))
+                == target;
+
+            if !updated_p1 && !has_concat && valid {
                 p1 += target;
+                updated_p1 = true;
+            }
+
+            if !updated_p2 && valid {
+                p2 += target;
+                updated_p2 = true;
+            }
+
+            if updated_p1 && updated_p2 {
                 break;
             }
         }
     }
-
-    let mut p2 = 0;
 
     let end = start.elapsed();
     println!("2024 Day 07:");
